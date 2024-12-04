8160f8d6f12af93f7935b1b8bb9ea81ce0b55c82
Ana Leonel <analeonel93@gmail.com>
2024-12-04 00:19:11 -0300

    Model criado

---
+++ TaskManager/Models/Task/TaskViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models.Task
+{
+    public class TaskViewModel
+    {
+        public int Id { get; set; } // identificador único
+        public string Title { get; set; } // titulo do ticket
+        public string Description { get; set; } // descrição do problema relacionado
+        public string Priority { get; set; } // prioridade (baixa, média, alta)
+        public string Status { get; set; } // status (aberto, em Andamento, fechado)
+        public DateTime DateOpened { get; set; } // data de abertura do ticket
+    }
+}
